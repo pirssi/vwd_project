90c1e98c430518d7f70c41dd40a3d5c7674e5e64
pirssi <janipir@uef.fi>
2021-02-15 17:15:38 +0200

    cleanup

--- jani.js
+++ jani.js
@@ -1,18 +1,30 @@
-const GRIDSIZEX = 100;
-const GRIDSIZEY = 100;
-const OBSTACLECOUNT = 50;
+const GRIDSIZEX = 10;
+const GRIDSIZEY = 10;
+const OBSTACLECOUNT = 5;
 
 function randomizePos() {
-  let obstaclePositions = new Array();
+  let obstaclePositions = new Array(); //array that has OBSTACLECOUNT number of x and y coordinates
 
   let i;
   for (i = 0; i < OBSTACLECOUNT; i++) {
+    //generate the position as an array
     let pos = [
-      Math.floor(Math.random() * GRIDSIZEX),
-      Math.floor(Math.random() * GRIDSIZEY),
+      Math.floor(Math.random() * GRIDSIZEX), //generate random x coordinate
+      Math.floor(Math.random() * GRIDSIZEY), //generate random y coordinate
     ];
-    obstaclePositions.push(pos);
+    obstaclePositions.push(pos); //push the array of a single position to the array with all the coordinates (nested arrays)
   }
-  console.log(obstaclePositions);
   return obstaclePositions;
+}
+function drawObstacles() {
+  let obstaclePositions = randomizePos();
+
+  let i;
+
+  for (i = 0; i < obstaclePositions.length; i++) {
+    // console.log(
+    //   "x: " + obstaclePositions[i][0] + ", y: " + obstaclePositions[i][1]
+    // );
+    //TODO: actual drawing
+  }
 }
