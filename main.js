b1ff74a164e598634ceb6042ed0af1c7553e13a9
nikoaro <nikoaro@uef.fi>
2021-03-12 11:17:57 +0200

    Added puttAudio and changed hitAudio and holeAudio

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ const SIZE = 1000;
 // set it every time ball stops
 var hitPosX;
 var hitPosY;
-var hitAudio = new Audio('http://sfxcontent.s3.amazonaws.com/soundfx/GolfSwing.mp3');
+var hitAudio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-adam-a-johnson/aaj_0384_Golf_Driver_04.mp3?_=1');
 hitAudio.volume = 0.3;
-var holeAudio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-31172/zapsplat_sport_golf_crazy_ball_drop_into_hole_006_31530.mp3?_=1');
+var puttAudio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-adam-a-johnson/aaj_0373_Golf_Putt_02.mp3?_=1');
+var holeAudio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-adam-a-johnson/aaj_0380_Golf_Ball_In_Hole1.mp3?_=1');
 var waterAudio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-one/water_splash_hand_slap.mp3?_=1'); 
 waterAudio.volume = 0.3; //doesnt change the volume for some reason
 var bounceAudio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-35448/zapsplat_sport_basketball_single_light_bounce_001_35731.mp3?_=1');
