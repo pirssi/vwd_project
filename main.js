b0e2805c7ad1d8834bd35373459d1bc9fa0f6a95
pirssi <janipir@uef.fi>
2021-03-16 21:39:37 +0200

    stored audio file sources locally

--- main.js
+++ main.js
@@ -4,26 +4,14 @@ const SIZE = 1000;
 // set it every time ball stops
 var hitPosX;
 var hitPosY;
-var hitAudio = new Audio(
-  "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-adam-a-johnson/aaj_0384_Golf_Driver_04.mp3?_=1"
-);
+var hitAudio = new Audio("audio/driver.mp3");
 hitAudio.volume = 0.3;
-var puttAudio = new Audio(
-  "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-adam-a-johnson/aaj_0373_Golf_Putt_02.mp3?_=1"
-);
-var holeAudio = new Audio(
-  "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-adam-a-johnson/aaj_0380_Golf_Ball_In_Hole1.mp3?_=1"
-);
-var waterAudio = new Audio(
-  "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-one/water_splash_hand_slap.mp3?_=1"
-);
+var puttAudio = new Audio("audio/putt.mp3");
+var holeAudio = new Audio("audio/hole.mp3");
+var waterAudio = new Audio("audio/splash.mp3");
 waterAudio.volume = 0.3; //doesnt change the volume for some reason
-var bounceAudio = new Audio(
-  "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-35448/zapsplat_sport_basketball_single_light_bounce_001_35731.mp3?_=1"
-);
-var cheerAudio = new Audio(
-  "https://freesound.org/data/previews/245/245639_591423-lq.mp3"
-);
+var bounceAudio = new Audio("audio/bounce.mp3");
+var cheerAudio = new Audio("audio/cheer.mp3");
 var bounceCD = 1;
 
 var horizontalVel;
